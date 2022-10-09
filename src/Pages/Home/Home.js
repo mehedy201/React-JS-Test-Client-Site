b1777b07aa28dd5205ab25f0741d91ea85e00745
mehedy201 <mehedy643@gmail.com>
2022-10-09 13:43:38 +0600

    created top section

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './Home.css'
+import { GrAdd } from "react-icons/gr";
+import { PlusIcon } from '@heroicons/react/24/solid'
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Mehedi</h1>
-            </div>
-        );
-    }
-}
+
+
+
+const Home = () => {
+    return (
+        <div className='flex justify-between mt-6'>
+            <h2 className="text-4xl font-bold">Student List</h2>
+            <button class="btn px-8 capitalize text-xl">
+                <PlusIcon className="h-6 w-6 text-white mr-3 font-bold"/>
+                 Add
+            </button>
+        </div>
+    );
+};
 
 export default Home;
